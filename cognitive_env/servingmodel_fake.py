4110ae743d2e26d12b3b399fdcb1e995a912cd9f
Emerson Faria de Oliveira <nosreme20133@hotmail.com>
2024-05-04 15:17:14 -0300

    Modelo fake adaptation

--- cognitive_env/servingmodel_fake.py
+++ cognitive_env/servingmodel_fake.py
@@ -21,6 +21,10 @@ app = Flask(__name__)
 app.json_encoder = NpEncoder
 modelo = None
 
+# Dados de exemplo
+grupos = ['Grupo A', 'Grupo B', 'Grupo C']
+personas = ['Conservador', 'Moderado', 'Dinâmico', 'Aventureiro']
+
 # Função para gerar uma lista de valores aleatórios
 def generate_random_list(size, lower=-1.0, upper=1.0):
     return [random.uniform(lower, upper) for _ in range(size)]
@@ -40,16 +44,23 @@ def call_predict(request = request):
     json_ = request.json
     campos = pd.DataFrame(json_)
 
+    # Seleciona um grupo e uma persona aleatoriamente
+    grupo = random.choice(grupos)
+    persona = random.choice(personas)
+    propensao_fraude = generate_random_list(1,0,1)
+    propensao_fraude = propensao_fraude[0]
+
     if campos.shape[0] == 0:
         return "Dados de chamada da API estão incorretos.", 400
 
-    # Gerar uma lista de 4 valores aleatórios
-    random_list = generate_random_list(4)
-    random_list
+    # Monta a resposta
+    resposta = {
+        'Grupo': grupo,
+        'Persona': persona,
+        'Propensão à Fraude': propensao_fraude
+    }
 
-    ret = {'prediction': random_list}
-
-    return app.response_class(response=json.dumps(ret, cls=NpEncoder), mimetype='application/json')
+    return app.response_class(response=json.dumps(resposta, cls=NpEncoder), mimetype='application/json')
 
 if __name__ == '__main__':
     args = sys.argv[1:]
